ee342c0dd93fab97c0b436b78c08798747153796
denghua <denghua.fm@foxmail.com>
2024-01-20 03:55:37 +0800

    comments auth.config.js

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,6 +1,8 @@
 const config = {
   providers: [
     {
+      // 本地开发时使用.env.local的环境变量
+      // 部署时需要配置在convex dashboard -> settings -> environment-variables中
       domain: process.env.CLERK_DEV_DOMAIN,
       applicationID: process.env.CLERK_APP_ID,
     }
